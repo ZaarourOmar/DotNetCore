1bb24726de0fb8855889caa1b57ded724e5dcc8b
Omar Zaarour <omarzaarour@nureva.com>
2017-08-08 10:15:07 -0600

    add bower and basic js

---
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿//site.js
+
+// We use anonymous function here to esnure that everything becomes in the private scope of this file.
+
+(function () {
+    var ele = document.getElementById("userName");
+    ele.innerHTML = "some other username";
+
+
+
+    var main = document.getElementById("main");
+
+    main.onmouseenter = function () {
+        main.style.background = "#888";
+    };
+
+
+    main.onmouseleave = function () {
+        main.style.background = "";
+    };
+})("some parameter"); // param is optional
