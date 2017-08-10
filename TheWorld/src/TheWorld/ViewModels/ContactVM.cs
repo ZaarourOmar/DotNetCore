884c9aeac69bcbcfed8b96ad5d5fd2360ccfde45
Omar Zaarour <omarzaarour@nureva.com>
2017-08-10 16:36:32 -0600

    add basic view model for contact page and bind to the view model properties

---
+++ TheWorld/src/TheWorld/ViewModels/ContactVM.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace TheWorld.ViewModels
+{
+    public class ContactVM
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
