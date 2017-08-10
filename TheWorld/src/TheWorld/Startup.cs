5d1769f90a5d72043ff7985c1afae0927df72ffd
Omar Zaarour <omarzaarour@nureva.com>
2017-08-09 22:44:08 -0600

    create simple controller and simple view and enable mvc6

--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -16,23 +16,24 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //dependency injection
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseDefaultFiles();
             app.UseStaticFiles();
-            app.Run(async (context) =>
+
+
+            app.UseMvc(cnfg =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                cnfg.MapRoute(
+                   name: "Default",
+                   template: "{controller}/{action}/{id?}",
+                   defaults: new { controller = "App", action = "Index" }
+                            );
             });
         }
     }
